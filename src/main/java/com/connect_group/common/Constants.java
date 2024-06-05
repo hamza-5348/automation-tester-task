40e7c32834e6057af39843a0674d5d3ec8c3cdab
Hamza Hassan <72945473+hamza-5348@users.noreply.github.com>
2024-06-05 17:49:41 +0100

    Delete src/main/java/com/connect_group/common directory

--- src/main/java/com/connect_group/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.connect_group.common;
-
-public final class Constants {
-
-//   public static final String NAME_PLATE_CONTAINER_CSS_SELECTOR = ".fc-nameplates__nameplateContainer";
-    public static final String NAME_PLATE_CONTAINER_CSS_SELECTOR = ".fc-nameplates__nameplates";
-}
